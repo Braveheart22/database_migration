fdf3f61c686066e94b1010cf794d5619150cdd19
John Strack <Braveheart22@gmail.com>
2026-02-26 13:58:54 -0500

    Added DBDrivers.py

---
+++ DBDrivers.py
@@ -0,0 +1,34 @@
+import pyodbc
+for driver in pyodbc.drivers():
+      print(driver)
+
+for DSN in pyodbc.dataSources():
+      print(DSN)
+
+# Using a DSN, but providing a password as well
+cnxn = pyodbc.connect('DSN=Catch22-17;UID=dba;PWD=sql34')
+
+# Create a cursor from the connection
+cursor = cnxn.cursor()
+# SELECTing data
+# cursor.execute("SELECT * FROM prep WHERE prep_id = (SELECT max(prep_id) FROM prep)")
+# row = cursor.fetchone()
+# if row:
+#     print(row)
+# cursor.execute("SELECT TOP 25 user_name, department FROM lll_users")
+# rows = cursor.fetchall()
+# for row in rows:
+#     print(row.user_name, row.department)
+
+# INSERTing data
+# cursor.execute("INSERT INTO bank(bank_id, name) VALUES ('bnk3', 'Bank Three')")
+cursor.execute(
+    "INSERT INTO bank (bank_id, name) VALUES (?, ?)",
+    ("bnk3", "Bank Three")
+)
+cnxn.commit()
+
+# DELETEing data
+# cursor.execute("DELETE FROM bank WHERE bank_id = ?", 'bnk3')
+# print(cursor.rowcount, 'rows deleted')
+# cnxn.commit()
