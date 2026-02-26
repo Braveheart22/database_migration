938e8d24464abd4d107bf978251b5cb7c04343a0
John Strack <Braveheart22@gmail.com>
2026-02-26 14:58:49 -0500

    Added MSSQL connection string to tableTransfer.py (for future)

--- DBDrivers.py
+++ DBDrivers.py
@@ -7,6 +7,14 @@ for DSN in pyodbc.dataSources():
 
 # Using a DSN, but providing a password as well
 cnxn = pyodbc.connect('DSN=Catch22-17;UID=dba;PWD=sql34')
+# For future MSSQL database
+# conn = pyodbc.connect(
+#     'DRIVER={ODBC Driver 18 for SQL Server};'
+#     'SERVER=Loop-DB01;'
+#     'DATABASE=LoopLoc_SchemaOnly;' # Subject to change when final db is up and running
+#     'Trusted_Connection=yes;'
+#       'TrustServerCertificate=yes;'
+# )
 
 # Create a cursor from the connection
 cursor = cnxn.cursor()
